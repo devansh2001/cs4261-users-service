260c16afaebd7c707d21c91bafad874723c5139f
Devansh Jatin Ponda <devanshponda@gmail.com>
2021-10-05 03:15:57 -0400

    added stubbed methods

--- app.py
+++ app.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 def health_check():
     return {'status': 'OK', 'code': '200'}
 
+@app.route('/create-user')
+def create_user():
+    return {'status': 201}
+
+@app.route('/get-user/<user_id>')
+def get_user(user_id):
+    return {'status': 200}
+
+@app.route('/delete-user/<user_id>')
+def delete_user(user_id):
+    return {'status': 200}
+
+@app.route('/authenticate')
+def authenticate():
+    return {'status': 200}
+
 # https://www.youtube.com/watch?v=4eQqcfQIWXw
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
